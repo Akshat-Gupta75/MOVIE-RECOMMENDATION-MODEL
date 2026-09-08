9f497abbf57596e920e0a43d22d9929e56c0158d
Akshat-Gupta75 <Akshatgupta7509@gmail.com>
2026-09-08 14:33:55 +0530

    Update app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # =============================
 # CONFIG
 # =============================
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = "https://movie-recommendation-model-uoi3.onrender.com"or"http://127.0.0.1:8000"
 
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
